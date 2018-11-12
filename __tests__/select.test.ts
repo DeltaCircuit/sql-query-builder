6a0fe36491fb83b8b00bfb4fbd703ad5fc079aac
Giridharan GM <giridharangm@outlook.com>
2018-11-12 19:22:28 +0530

    Enable column aliasing

--- __tests__/select.test.ts
+++ __tests__/select.test.ts
@@ -1,6 +1,24 @@
 import builder from "../src/";
 
-it("should return select * ", () => {
-  const query = builder({ columns: "*", table: "users", type: "select" });
-  expect(query).toEqual("select * from users");
+describe("Different type of selects", () => {
+  it("select * ", () => {
+    const query = builder({ columns: "*", table: "users", type: "select" });
+    expect(query).toEqual("select * from users");
+  });
+  it("select array of columns ", () => {
+    const query = builder({
+      columns: ["id", "name"],
+      table: "users",
+      type: "select"
+    });
+    expect(query).toEqual("select id, name from users");
+  });
+  it("select columns with alias ", () => {
+    const query = builder({
+      columns: [{ id: "idAlias" }, { name: "nameAlias" }],
+      table: "users",
+      type: "select"
+    });
+    expect(query).toEqual("select id as idAlias, name as nameAlias from users");
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
 interface QueryObject {
   type: "select" | "update" | "delete" | "insert";
   table: string;
-  columns: string | Array<string>;
+  columns: string | Array<string | object>;
 }
-const buildQuery = (query: Partial<QueryObject>) => {
-  return `${query.type} ${query.columns} from ${query.table}`;
+
+const buildSelectColumns = (
+  selectPart: string | Array<string | object>
+): string => {
+  if (Array.isArray(selectPart)) {
+    const columns = selectPart.map((columnName: any) => {
+      if (typeof columnName === "object") {
+        const [name] = Object.keys(columnName);
+        return `${name} as ${columnName[name]}`;
+      }
+      return columnName;
+    });
+    return columns.join(", ");
+  }
+  return selectPart;
+};
+const buildQuery = (query: QueryObject) => {
+  return `${query.type} ${buildSelectColumns(query.columns)} from ${
+    query.table
+  }`;
 };
 export default buildQuery;
